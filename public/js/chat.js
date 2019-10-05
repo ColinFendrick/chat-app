0e641f48a1f45b9af5a384780757e8622f1bfa52
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-05 12:40:39 -0400

    Add login/logout emits

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,7 +2,7 @@ const socket = io()
 
 document.querySelector('#message-form').addEventListener('submit', e => {
   e.preventDefault()
-  const message = document.querySelector('input').value
+  const message = e.target.elements.message.value
   socket.emit('sendMessage', message)
 })
 
