f5597fe90539da205230ad92c56fb26eff948c12
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-06 09:59:04 -0400

    Track users on sidebar

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages')
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -30,6 +31,11 @@ socket.on('locationMessage', ({ username, locationUrl, createdAt }) => {
     createdAt: moment(createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, { room, users })
+  document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', e => {
